9c9b1086bc86b4b718922c440df5a204b40aa40c
Korthax <stephenneilphillips@gmail.com>
2017-02-12 17:18:01 +0000

    Added Cake.Path addin

---
+++ src/Cake.Path/EnvironmentWrapper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Cake.Path
+{
+    public class EnvironmentWrapper : IEnvironmentWrapper
+    {
+        public string GetEnvironmentVariable(string variable, string @default)
+        {
+            return Environment.GetEnvironmentVariable(variable) ?? @default;
+        }
+
+        public void SetEnvironmentVariable(string variable, string value)
+        {
+            Environment.SetEnvironmentVariable(variable, value);
+        }
+    }
+}
